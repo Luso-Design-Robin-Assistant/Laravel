e76491fb7ce8f02b11dcfe65a136537e3b632f6b
Gijs Huitenga <gijs.huitenga@gmail.com>
2021-03-11 23:45:15 +0100

    Eerste versie header toegevoegd
    Een transparente header toegevoegd die wit word zodra er word gescrolled.

--- robin-assistant/resources/js/app.js
+++ robin-assistant/resources/js/app.js
@@ -1,1 +1,31 @@
 require('./bootstrap');
+
+//
+//  Project: Robin Assistant
+//  Created by: Matthijs Berghuis, Gijs Huitenga
+//  Last Edited: 11.03.2021
+//
+//  Sections:
+//  =============
+//  1. Header onScroll
+//  2. 
+//  =============
+// 
+
+//
+//  1. Header onScroll
+//
+
+$(function() {
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > 50) {
+            $(".navbar").addClass("navbar-scroll");
+            $(".nav-link").addClass("nav-link-scroll");
+            $(".nav-login-btn").addClass("nav-login-btn-scroll");
+        } else {
+            $(".navbar").removeClass("navbar-scroll");
+            $(".nav-link").removeClass("nav-link-scroll");
+            $(".nav-login-btn").removeClass("nav-login-btn-scroll");
+        }
+    });
+});
---
+++ robin-assistant/resources/views/index.blade.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Robin assistant</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('../css/app.css') }}">
+    </head>
+
+    <body>
+
+        <!-- Navbar -->
+        <nav class="navbar">
+            <div class="navbar-content">
+                <div class="navbar-logo">
+                    <img src="" alt="">
+                </div>
+                <div class="navbar-menu">
+                    <div class="navbar-links">
+                        <a class="nav-link" href="#what-is-robin">What is Robin?</a>
+                        <a class="nav-link" href="#benefits">Benefits</a>
+                        <a class="nav-link" href="#how-it-works">How does it work?</a>
+                        <a class="nav-link" href="#faq">FAQ</a>
+                    </div>
+                    <div class="navbar-login">
+                        <div class="nav-profile-img"></div>
+                        <a class="nav-login-btn" href="#login">Inloggen</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <!-- Main content -->
+        <div class="main">
+            <div class="hero-image">
+
+            </div>
+            <div class="main-content">
+
+            </div>
+        </div>
+
+    </body>
+
+    <!-- Scripts -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="{{ asset('../js/app.js') }}"></script>
+
+</html>
